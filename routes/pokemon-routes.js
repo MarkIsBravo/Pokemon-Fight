54bf47ac1bfb289f13e19d0578d50869cb74c6b9
Mark Ma <mazheng1022@hotmail.com>
2017-08-01 12:44:24 -0400

    pokemon-routes structured

---
+++ routes/pokemon-routes.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const todoRoutes=express.Router();
+const authHelpers=require('../services/auth/auth-helpers');
+
+const pokemonsController=require('../controllers/pokemons-controller');
+
+pokemonRoutes.get('/',pokemonsController.index);
+pokemonRoutes.post('/',authHelpers.loginRequired,pokemonsController.create);
+
+pokemonRoutes.get('/add',authHelpers.loginRequired,(req,res)=>{
+    res.render('pokemons/pokemon-add',{
+        currentPage:'add',
+    });
+});
+
+pokemonRoutes.get('/:id',pokemonsController.show);
+
+module.exports=pokemonRoutes;
