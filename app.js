9ba9a661ace28cd37aa6e42914356e2c6820ec05
Mark Ma <mazheng1022@hotmail.com>
2017-08-01 12:18:33 -0400

    app.js structured

--- app.js
+++ app.js
@@ -6,3 +6,47 @@ const methodOverride=require('method-override');
 const cookieParser=require('cookie-parser');
 const session=require('express-session');
 const passport=require('passport');
+
+const app=express();
+require('dotenv').config();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(session({
+    secret:process.env.SECRET_KEY,
+    resave:false,
+    saveUnitialized:true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static('public'));
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+const port=process.env.PORT||3000;
+app.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+});
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+        message:'Welcome, trainer!',
+        currentPage:'home',
+    });
+});
+
+const pokemonRoutes=require('./routes/pokemon-routes');
+app.use('/pokemons',pokemonRoutes);
+const authRoutes=require('./routes/auth-routes');
+app.use('/auth',authRoutes);
+const userRoutes=require('./routes/user-routes');
+app.use('/user',userRoutes);
+
+app.get('*',(req,res)=>{
+    res.status(404).send('not found!');
+})
