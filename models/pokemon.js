97a0e516e5ea2bf09feae344631a8f6a26a81b81
Mark Ma <mazheng1022@hotmail.com>
2017-08-01 14:38:25 -0400

    pokemon-model structured

---
+++ models/pokemon.js
@@ -0,0 +1,25 @@
+const db=require('../db/config');
+
+const Pokemon={};
+
+Pokemon.findAll=()=>{
+    return db.query('SELECT * FROM pokemons');
+}
+
+Pokemon.findById=(id)=>{
+    return db.oneOrNone(`
+    SELECT * FROM pokemons
+    WHERE id = $1
+    `,[id]);
+}
+
+Pokemon.create=(pokemon,userid)=>{
+    return db.one(`
+    INSERT INTO pokemons
+    (name,type,attack,defense,user_id)
+    VALUES($1,$2,$3,$4,$5)
+    RETURNING *
+    `,[pokemon.name,pokemon.type,pokemon.attack,pokemon.defense,userid])
+}
+
+module.exports=Pokemon;
